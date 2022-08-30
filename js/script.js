c0c7839c40a70b3d0b9137357782b36c7c632a14
AlessandroVola <volaalessandro@gmail.com>
2022-08-30 11:04:54 +0200

    correzioni

--- js/script.js
+++ js/script.js
@@ -17,8 +17,7 @@ for (let i = 0; i < images.length; i++) {
     const image = images[i];
     const imageClass = i === activeIndex ? 'item active' : 'item'
 
-    //  container.innerHTML += '<img class="'+ imageClass +'" src="img/'+ image +'.jpg" alt="due" />'
-    // con il backtick è più bello da vedere
+
     container.innerHTML += `<img
                                 class="${imageClass}"
                                 src="img/${image}.jpg" alt="${image}" 
@@ -37,69 +36,41 @@ const previousButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
 
 // bottone immagine successiva
-nextButton.addEventListener('click',
-    function () {
-        console.log('mi hai cliccato!');
+nextButton.addEventListener('click', function () {
+    console.log('mi hai cliccato!');
 
-        /*
-        // versione senza indice
-        // recupero l'elemento attivo
-        const activeItem = document.querySelector('.item.active');
-        console.log(activeItem);
-        const nextElement = activeItem.nextElementSibling;
-        console.log(nextElement);
-        if (nextElement){
-            activeItem.classList.remove('active');
-            nextElement.classList.add('active');
-        }
-        */
-        if (activeIndex < images.length - 1) {
-            container.innerHTML = '';
-            activeIndex++;
 
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'item active' : 'item'
+    if (activeIndex < images.length - 1) {
+        container.innerHTML = '';
+        activeIndex++;
 
-                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const imageClass = i === activeIndex ? 'item active' : 'item'
 
-            }
+            container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+
         }
     }
+}
 );
 
 
 // bottone immagine precedente
-previousButton.addEventListener('click',
-    function () {
-        console.log('mi hai cliccato!');
-        /*
-        // versione senza indice
-        // recupero l'elemento attivo
-        const activeItem = document.querySelector('.item.active');
-        console.log(activeItem);
-        const previousElement = activeItem.previousElementSibling;
-        console.log(previousElement);
-        if (previousElement){
-            activeItem.classList.remove('active');
-            previousElement.classList.add('active');
-        }
-        */
+previousButton.addEventListener('click', function () {
+    console.log('mi hai cliccato!');
 
-        // la versione con activeIndex sarà uguale a quanto fatto per il nextButton eccetto
-        // - controllo activeIndex > 0 (ci deve essere qualcosa dopo che ho sottratto!)
-        // - decremento dell'indice con activeIndex--
-        if (activeIndex > 0) {
-            container.innerHTML = '';
-            activeIndex--;
+    if (activeIndex > 0) {
+        container.innerHTML = '';
+        activeIndex--;
 
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'item active' : 'item'
+        for (let i = 0; i < images.length; i++) {
+            const image = images[i];
+            const imageClass = i === activeIndex ? 'item active' : 'item'
 
-                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+            container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
 
-            }
         }
     }
+}
 );
